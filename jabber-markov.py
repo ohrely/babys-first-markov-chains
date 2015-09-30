c319f114251d70583b9b09650606c0b16f90eed5
Hackbright Student <info@hackbrightacademy.com>
2015-09-30 13:13:06 -0700

    Added comments to jabber-makov

--- jabber-markov.py
+++ jabber-markov.py
@@ -2,55 +2,57 @@ from random import choice
 
 
 def open_and_read_file(file_path):
-    """Takes file path as string; returns text as string.
+    """Takes file path as string; returns entire text as one list of words.
 
     Takes a string that is a file path, opens the file, and turns
-    the file's contents as one string of text.
+    the file's contents into a list of words.
     """
 
-    with open(file_path) as source_text:
+    with open(file_path) as source_text: # opens file as a whole, not line by line
         read_text = source_text.read()
         text = read_text.split()
     return text
 
 
-def make_chains(text_string):
-    """Takes input text as string; returns dictionary of markov chains.
+def make_chains(text_list):
+    """Takes input text as list; returns dictionary of markov chains.
 
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
-    words in the input text.
+    A chain will be a key that consists of a single word (type string)
+    and the value would be a list of the word(s) that follow that
+    word in the input list.
 
     For example:
 
-        >>> make_chains("hi there mary hi there juanita")
-        {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
+        >>> make_chains("hi mary, hi juanita")
+        {'hi': ['mary', 'juanita'], ('mary,': ['hi']}
     """
 
     chains = {}
 
-    for index in range(len(text_string)-1):
-        current_key = (text_string[index])
-        value_to_add = text_string[index + 1]
+    for index in range(len(text_list) - 1): #doesn't use last word as a key because no value exists
+        current_key = text_list[index]
+        value_to_add = text_list[index + 1] #each word's value is the word that immediately follows
         
         if current_key in chains:
             chains[current_key].append(value_to_add)
         else:
-            chains[current_key] = [value_to_add]
+            chains[current_key] = [value_to_add] #adds value in list so that more values can be added
     return chains
 
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
 
-    first_key = choice(chains.keys())
-    text = first_key[0]
+    #randomly chooses first word from all keys, adds to text, and sets as the key to use
+    first_key = choice(chains.keys()) #choose first key from list of keys
+    text = first_key
     new_key = first_key
 
-    while new_key in chains:
-        next_word = choice(chains[new_key])
+    #uses chains dictionary to select next word, then uses next word as key to choose next word
+    while new_key in chains: #will stop when reaches last word because it is not a key
+        next_word = choice(chains[new_key]) #randomly selects from all possible values at key
         text = text + " " + next_word
-        new_key = (next_word)
+        new_key = next_word #last word in text becomes key
 
     print text
 
